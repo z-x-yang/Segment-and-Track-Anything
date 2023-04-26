bcd5c0ddda3658d94eb03fd02a127b998c71367b
yamy-cheng <qiangming0313@gmail.com>
2023-04-26 21:52:49 +0800

    fix git clone

--- script/install.sh
+++ script/install.sh
@@ -4,7 +4,7 @@ cd -
 
 # Install Grounding-Dino
 cd src
-git clone git@github.com:yamy-cheng/GroundingDINO.git
+git clone https://github.com/yamy-cheng/GroundingDINO.git
 cd GroundingDINO
 pip install -e .
 cd ../..
