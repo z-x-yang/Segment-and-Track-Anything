a9366b3c802f7431f534331efef6dbb91954d461
dsigmabcn <92378590+dsigmabcn@users.noreply.github.com>
2023-09-24 18:33:31 +0200

    Update install.sh
    re-install gradio for webui and version back imageio to 2.26

--- script/install.sh
+++ script/install.sh
@@ -8,6 +8,8 @@ pip install -e git+https://github.com/IDEA-Research/GroundingDINO.git@main#egg=G
 # Install other lib
 pip install numpy opencv-python pycocotools matplotlib Pillow scikit-image
 pip install gradio zip gdown ffmpeg
+pip install gradio ##### for some reason, in the jupyter notebook needs to be installed again
+pip install imageio==2.26.0 #the current code does not work with imageio, need to install old version
 
 # Install Pytorch Correlation
 git clone https://github.com/ClementPinard/Pytorch-Correlation-extension.git
