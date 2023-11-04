c0b9f41f5f6f9cba61807c0ac6451c64d4d71b25
yamy-cheng <91768613+yamy-cheng@users.noreply.github.com>
2023-11-04 12:32:47 +0800

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -6,8 +6,8 @@ cd -
 pip install -e git+https://github.com/IDEA-Research/GroundingDINO.git@main#egg=GroundingDINO
 
 # Install other lib
-pip install numpy opencv-python pycocotools matplotlib Pillow scikit-image
-pip install gradio zip gdown ffmpeg
+pip install numpy opencv-python pycocotools matplotlib Pillow==9.2.0 scikit-image
+pip install gradio==3.28.1 zip gdown ffmpeg==1.4
 
 # Install Pytorch Correlation
 git clone https://github.com/ClementPinard/Pytorch-Correlation-extension.git
