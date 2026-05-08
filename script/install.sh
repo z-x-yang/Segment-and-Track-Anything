630b5949c897528bd41691bbf8c5dbbab65999eb
varun sai <varunsaip2005@gmail.com>
2026-05-08 11:17:33 +0530

    fixed dependencies

--- script/install.sh
+++ script/install.sh
@@ -1,15 +1,28 @@
+# better create a virtual environment with python 3.10 especially and activate it
+py -3.10 -m venv venv
+source venv/Scripts/activate
+pip install --upgrade pip setuptools wheel
+# install torch and torchvision manually if you have cuda or cpu, according to the cuda version
+# from here https://pytorch.org/get-started/locally/
+
 # Install SAM
 cd sam; pip install -e .
 cd ..
 
 # Install Grounding-Dino
-pip install -e git+https://github.com/IDEA-Research/GroundingDINO.git@main#egg=GroundingDINO
+git clone -b main https://github.com/IDEA-Research/GroundingDINO.git
+cd GroundingDINO
+grep -vE "torch|torchvision" requirements.txt > require.txt
+pip install -r require.txt
+pip install -e . --no-build-isolation
+cd ..
 
 # Install other lib
 pip install numpy opencv-python pycocotools matplotlib Pillow==9.2.0 scikit-image
-pip install gradio==3.39.0 zip gdown ffmpeg==1.4
+pip install gradio==3.39.0 gdown ffmpeg==1.4
 pip install timm==0.4.5
 pip install wget
+pip install moviepy==1.0.3
 
 # Install Pytorch Correlation
 git clone https://github.com/ClementPinard/Pytorch-Correlation-extension.git
