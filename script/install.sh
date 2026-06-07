c7460ea34ba46e919e202bd26fa1cf403705c27f
Varun Sai <varunsaip2005@gmail.com>
2026-06-07 22:07:35 +0530

    Updated install.sh

--- script/install.sh
+++ script/install.sh
@@ -14,17 +14,17 @@ git clone -b main https://github.com/IDEA-Research/GroundingDINO.git
 # install torch, torchvision and torchaudio manually if you have cuda or cpu, according to the cuda version
 # from here https://pytorch.org/get-started/locally/
 
-transformers==4.30.2
-addict==2.4.0
-yapf==0.40.2
-timm==0.4.5
-numpy==1.26.4
-opencv-python==4.10.0.84
-Pillow==10.4.0
-scikit-image==0.24.0
-matplotlib==3.9.2
-supervision==0.22.0
-pycocotools==2.0.8
+pip install transformers==4.30.2
+pip install addict==2.4.0
+pip install yapf==0.40.2
+pip install timm==0.4.5
+pip install numpy==1.26.4
+pip install opencv-python==4.10.0.84
+pip install Pillow==10.4.0
+pip install scikit-image==0.24.0
+pip install matplotlib==3.9.2
+pip install supervision==0.22.0
+pip install pycocotools==2.0.8
 
 cd GroundingDINO
 pip install -e . --no-build-isolation
@@ -32,7 +32,8 @@ cd ..
 
 # Install other lib
 pip install scikit-image
-pip install gradio==3.39.0 wget gdown
+pip install gradio==3.39.0
+pip install gradio_client==0.5.0 wget gdown
 pip install timm==0.4.5
 pip install moviepy==1.0.3
 
