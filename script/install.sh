251f5de827ebf191f85930563851ee935656624c
Varun Sai <varunsaip2005@gmail.com>
2026-06-28 13:05:12 +0530

    updated install.sh

--- script/install.sh
+++ script/install.sh
@@ -1,10 +1,10 @@
 # better create a virtual environment with python 3.10 especially and activate it
 python3.10 -m venv venv
 source venv/bin/activate
-pip install --upgrade pip wheel setuptools==80.9.0
+python -m pip install --upgrade pip wheel setuptools==80.9.0
 
 # Install SAM
-cd sam; pip install -e .
+cd sam; python -m pip install -e .
 cd ..
 
 # Install Grounding-Dino. IF you get that "Failed to load custom C++" refer to
@@ -20,25 +20,25 @@ git clone -b main https://github.com/IDEA-Research/GroundingDINO.git
 # The versions below (2.0.1 / 0.15.2 / 2.0.2) are recommended and should be kept the same.
 # Only change the wheel index (cu118, cu121, cpu, etc.).
 
-pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118
-pip install transformers==4.30.2
-pip install addict==2.4.0
-pip install yapf==0.40.2
-pip install timm==0.4.5
-pip install numpy==1.26.4
-pip install opencv-python==4.10.0.84
-pip install Pillow==10.4.0
-pip install scikit-image==0.24.0
-pip install matplotlib==3.9.2
-pip install supervision==0.22.0
-pip install pycocotools==2.0.8
+python -m pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118
+python -m pip install transformers==4.30.2
+python -m pip install addict==2.4.0
+python -m pip install yapf==0.40.2
+python -m pip install timm==0.4.5
+python -m pip install numpy==1.26.4
+python -m pip install opencv-python==4.10.0.84
+python -m pip install Pillow==10.4.0
+python -m pip install scikit-image==0.24.0
+python -m pip install matplotlib==3.9.2
+python -m pip install supervision==0.22.0
+python -m pip install pycocotools==2.0.8
 
 cd GroundingDINO
-pip install -e . --no-build-isolation
+python -m pip install -e . --no-build-isolation
 cd ..
 
 # Install other lib
-pip install \
+python -m pip install \
 gradio==3.39.0 \
 gradio_client==0.5.0 \
 pydantic==1.10.13 \
@@ -46,9 +46,9 @@ fastapi==0.100.1 \
 starlette==0.27.0 \
 wget \
 gdown
-pip install moviepy==1.0.3
+
+python -m pip install moviepy==1.0.3
 
 # Install AST
 git clone https://github.com/YuanGongND/ast.git ast_master
 cp ./prepare.py ./ast_master
-
