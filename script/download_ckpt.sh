11f204d5f20e30ce641c2e7c042cb289111581c1
Varun Sai <varunsaip2005@gmail.com>
2026-07-04 10:47:20 +0530

    added checkpoint folders to download_ckpt.sh

--- script/download_ckpt.sh
+++ script/download_ckpt.sh
@@ -1,4 +1,4 @@
-# download aot-ckpt 
+# download aot-ckpt
 gdown --id '1QoChMkTVxdYZ_eBlZhK2acq9KMQZccPJ' --output ./ckpt/R50_DeAOTL_PRE_YTB_DAV.pth
 
 # download sam-ckpt
@@ -8,4 +8,7 @@ wget -P ./ckpt https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.
 wget -P ./ckpt https://huggingface.co/ShilongLiu/GroundingDINO/resolve/main/groundingdino_swint_ogc.pth
 
 # download mit-ast-finetuned ckpt
-wget -O ./ast_master/pretrained_models/audio_mdl.pth https://www.dropbox.com/s/cv4knew8mvbrnvq/audioset_0.4593.pth?dl=1
+wget -O ./ckpt/audio_mdl.pth https://www.dropbox.com/s/cv4knew8mvbrnvq/audioset_0.4593.pth?dl=1
+
+wget -O ./ckpt/class_labels_indices.csv \
+https://raw.githubusercontent.com/YuanGongND/ast/master/egs/audioset/data/class_labels_indices.csv
